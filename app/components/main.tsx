7d191f895374d542290494cea5c4781248d9495f
Tony Haenn <tonyhaenn@gmail.com>
2021-04-29 00:06:43 +0000

    Tailwind WIP

---
+++ app/components/main.tsx
@@ -0,0 +1,18 @@
+
+
+  
+export default function Main(props){
+  return (
+    <div>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          {/* Replace with your content */}
+          <div className="px-4 py-6 sm:px-0">
+            {props.children}
+          </div>
+          {/* /End replace */}
+        </div>
+      </main>
+    </div>
+  )
+}
