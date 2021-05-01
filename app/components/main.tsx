f5e6e83e86f2ae0339402c7e646ec7c5d4a30880
Tony Haenn <tonyhaenn@gmail.com>
2021-05-01 01:27:11 +0000

    FE WIP

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -7,7 +7,7 @@ export default function Main(props){
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           {/* Replace with your content */}
-          <div className="px-4 py-6 sm:px-0">
+          <div className="flex px-4 sm:px-0"> 
             {props.children}
           </div>
           {/* /End replace */}
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import Nav from '../components/nav'
 import Main from '../components/main'
+import Hero from '../components/hero'
 
 export default function Home() {
   return (
     <div>
+      
       <Nav />
-      <Main />
+      <Main>
+        <Hero />
+      </Main>
     </div>
   )
 }
