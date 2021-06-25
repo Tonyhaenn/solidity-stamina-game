095c3d87cd2260e6a5ec95516cc030fa5372badb
Tony Haenn <tonyhaenn@gmail.com>
2021-06-25 12:59:14 +0000

    WIP - adding connected state to buttons

--- app/components/walletProviders.tsx
+++ app/components/walletProviders.tsx
@@ -1,5 +1,8 @@
 
 import { useWeb3React } from '@web3-react/core';
+import { AbstractConnector } from '@web3-react/abstract-connector'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+
 import { injected, walletlink, walletconnect } from '../utils/connectors';
 import { useState, useEffect } from 'react';
 import { useInactiveListener, useEagerConnect }  from '../utils/hooks'
@@ -25,43 +28,66 @@ export default function WalletProviders() {
 
   const triedEager = useEagerConnect()
   useInactiveListener(!triedEager || !!activatingConnector)
-
-  
+ 
   return (
     <div>
       <h2 className="text-gray-500 text-xs font-medium uppercase tracking-wide">Wallets</h2>
       <ul className="mt-3 grid grid-cols-1 gap-5 sm:gap-6">
-        {wallets.map((wallet) => (
-          <li key={wallet.name} className="col-span-1 flex shadow-sm rounded-md">
-            <div
-              className={classNames(
-                wallet.bgColor,
-                'flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'
-              )}
-            >
-              {wallet.initials}
-            </div>
-            <button onClick={()=>{activate(wallet.connector)}} className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-              <div className="flex-1 px-4 py-4 text-sm truncate">
-                <span className="text-gray-900 font-medium hover:text-gray-600">
-                  {wallet.name}
-                </span>
-              </div>
-            </button>
-          </li>
+        {wallets.map((wallet)=>(
+          <WalletButton key={wallet.name} wallet={wallet} />
         ))}
       </ul>
     </div>
   )
 }
 
-function WalletButton(wallet){
-  const { connector } = useWeb3React();
+function WalletButton({wallet}){
+  const { connector, active, activate, deactivate } = useWeb3React();
 
-  /*
-  Rough idea. Encapsulate all the state for the button
-  1) Default state, not connected. Tapping button initiates connection 
-  2) Conncted state. Tapping button disconnects / tears down connection
-  */
+  function walletButtonClick(active, connector) {
+    //If we're active, tear it down
+    if(active){
+      deactivate();
+      resetWalletConnector(connector);
+      return;
+    }
+    //Else activate!
+    activate(connector)
+    return;
+  }
+
+  const resetWalletConnector = (connector: AbstractConnector) => {
+    if (
+      connector &&
+      connector instanceof WalletConnectConnector &&
+      connector.walletConnectProvider?.wc?.uri
+    ) {
+      connector.walletConnectProvider = undefined
+    }
+  }
+  
+  return (
+     <button className="col-span-1 flex shadow-sm rounded-md" onClick={()=>walletButtonClick(active, wallet.connector)}>   
+        <div
+        className={classNames(
+          wallet.bgColor,
+          'flex-shrink-0 border-b border-gray-200 py-6 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'
+        )}>
+          {wallet.initials}
+        </div>
+        <div className="flex-1 px-4 py-4 text-sm flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+          <div className="flex flex-col px-4 text-sm truncate">
+            <span className="text-gray-900 font-medium hover:text-gray-600">
+              {wallet.name}
+            </span>
+            <span className={classNames("text-gray-400 font-light italic")}>
+              Connected
+            </span>
+          </div> 
+        </div> 
+      </button>
+  )
+  
 
 }
+
